14e599026c08a1ff82bb5a2fbf21acb9fd95d907
YutaMikome <edamame329@gmail.com>
2017-06-06 13:35:14 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only:[:edit, :update]
-  resources :messages, only: :index
+  resources :groups, only: [:new, :edit] do
+    resources :messages, only: :index
+  end
 end
