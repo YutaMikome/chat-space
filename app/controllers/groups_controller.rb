06817a57579ebf17e386e6072f2cc840840e0c18
YutaMikome <edamame329@gmail.com>
2017-06-07 19:22:13 +0900

    group_create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,27 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
-    # binding.pry
   end
 
   def edit
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      flash[:notice] = "チャットグループが作成されました。"
+      redirect_to root_path
+    else
+      flash[:alert] = "チャットグループの作成に失敗しました。"
+      render :new
+    end
+  end
+
+
   private
   def group_params
-    params.permit(:name)
+    params.require(:group).permit(:name)
   end
 end
