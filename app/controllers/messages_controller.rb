68edf6a1d8aed60d262349bc9ce75a58e7b25e70
YutaMikome <edamame329@gmail.com>
2017-06-10 20:05:12 +0900

    Send_Message var3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
-  before_action :get_group, only:[:index, :create]
+  before_action :get_group, only: [:index, :create]
+
   def index
     @message = Message.new
   end
 
   def create
-    @message = Message.new(message_params)
-
+    @message = @group.messages.new(message_params)
     if @message.save
       flash[:notice] = "メッセージ投稿が完了しました。"
       redirect_to group_messages_path
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
   def get_group
