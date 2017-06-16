0a282f438125ebd05ac5f4223f7d2f89480253f0
YutaMikome <edamame329@gmail.com>
2017-06-16 18:39:29 +0900

    Asynchronous communication commit2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,22 +2,23 @@ $(function() {
   function buildHTML(message) {
     // var create_message = $(
       // '<div class="chat-screen__chat-area">' +
-    var name = '<p class="chat-screen__chat-area__message-name">'+ message.name +'</p>'
-    var time = '<p class="chat-screen__chat-area__message-time">'+ message.time + '<p>'
-    var body = '<p class ="chat-screen__chat-area__message">' + message.body +'</p>'
+    var name = '<p class="chat-screen__chat-area__message-name">'+ message.name +'</p>';
+    var time = '<p class="chat-screen__chat-area__message-time">'+ message.time + '<p>';
+    var body = '<p class ="chat-screen__chat-area__message">' + message.body +'</p>';
     if( message.image ) {
-      var image_url = '<img src = "' + message.image + '">'
-      var image = '<p class ="chat-screen__chat-area__image">' + image_url +'</p>'
+      var image_url = '<img src = "' + message.image + '">';
+      var image = '<p class ="chat-screen__chat-area__image">' + image_url +'</p>';
+    } else {
+      var image = '<p class ="chat-screen__chat-area__image"></p>';
     };
-    var image = '<p class ="chat-screen__chat-area__image"></p>'
     $('.chat-screen__chat-area').append(name, time, body, image);
-  }
+  };
 
   $('.message-form'). on('submit', function(e){
     e.preventDefault();
     e.stopPropagation();
     var formData = new FormData ($(this).get(0));
-    var url = $(this).attr("action")
+    var url = $(this).attr("action");
     $.ajax ({
       type:'POST',
       url: url,
@@ -28,7 +29,9 @@ $(function() {
     })
     .done(function(message) {
       buildHTML(message);
-      textField.val('');
+      // textField.val('');
+      $('.message-form')[0].reset();
+      $('.chat-screen__chat-area').animate({scrollTop: $('.chat-screen__chat-area')[0].scrollHeight}, 'fast');
     })
     .fail(function() {
       alert('error');
