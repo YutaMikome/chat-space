1361bd5ed9f231e0465c50944cbf5bb6b88331fc
YutaMikome <edamame329@gmail.com>
2017-06-17 17:30:47 +0900

    Incremental_Search1

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,31 @@
+// $(document).on('turbolinks:load', function(e){ //リロードしなくてもjsが動くようにする
+$(function() {
+  function buildHTML(user) {
+    var users_name = '<div class="chat-group-user clearfix">' +
+                    '<p class="chat-group-user__name">' + user.name + '<p>' +
+                    '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id +'" data-user-name="'+ user.name +'">追加</a>' + '</div>';
+    return users_name;
+  };
+  $(document).on('keyup', '#user-search-field', function(e){
+    var input = $.trim($(this).val());
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      data: ('keyword=' + input),
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data){
+      $('#user-search-result').find('li').remove();
+      $('#user-search-result').find('hr').remove();
+
+      $(data).each(function(i, user){
+        $('#user-search-result').append('<li>' + buildHTML(user) + '</li>');
+      })
+    })
+    .fail(function() {
+        alert("メッセージを入力してください。");
+    });
+  });
+});
